1e7fc6753bbe219f9977a37e643b4537cb049939
Georges Mainville <g.mainville@mphase.ca>
2024-01-13 16:36:32 -0500

    Add websocketing

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,67 @@
-import type { Response } from 'express';
+import type {
+  Request, Response, Application,
+} from 'express';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+import express from 'express';
 
-const express = require('express');
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
 
-const app = express();
-const port = 5001;
+interface ClientToServerEvents {
+  hello: () => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
+
+const port = process.env.PORT || 5000;
+const origin = process.env.ORIGIN || 'http://localhost:3000';
+
+const app: Application = express();
+const server = createServer(app);
+const io = new Server<
+ClientToServerEvents,
+ServerToClientEvents,
+InterServerEvents,
+SocketData
+>(server, {
+  cors: {
+    origin,
+    methods: ['GET', 'POST'],
+  },
+  transports: ['websocket'],
+});
 
 app.get('/', (req: Request, res: Response) => {
+  console.log('GET /', req.body);
   res.send('See you space cowboy...');
 });
 
 app.listen(port);
+
+io.on('connection', (socket) => {
+  console.log('client connected: ', socket.id);
+
+  socket.join('clock-room');
+
+  socket.on('disconnect', (reason) => {
+    console.log(reason);
+  });
+});
+
+setInterval(() => {
+  io.to('clock-room').emit('noArg');
+}, 1000);
 
 // eslint-disable-next-line no-console
 console.log(`[app] Running ... \n[app] Url: http://localhost:${port}`);
--- webapp/src/pages/game/game.tsx
+++ webapp/src/pages/game/game.tsx
@@ -1,8 +1,21 @@
 import { Container } from '@mui/material';
 import { FormattedMessage } from 'react-intl';
 import { Canvas } from '@react-three/fiber';
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 
 function Game() {
+  const [time, setTime] = useState('fetching');
+  useEffect(() => {
+    const socket = io('http://localhost:5000', { transports: ['websocket'] });
+    socket.on('connect', () => console.log(socket.id));
+    socket.on('connect_error', () => {
+      setTimeout(() => socket.connect(), 5000);
+    });
+    socket.on('time', (data) => setTime(data));
+    socket.on('disconnect', () => setTime('server disconnected'));
+  }, []);
+
   return (
     <Container>
       <FormattedMessage id="home.title" />
