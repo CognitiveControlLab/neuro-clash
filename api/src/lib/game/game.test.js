58afd278ac64855a5c1eaa31bb6cc05729a417a3
Etienne Simoneau <etiennesimoneau@hotmail.fr>
2024-01-14 11:38:35 -0500

    Add failing test to validate workflow

--- api/src/lib/game/game.test.js
+++ api/src/lib/game/game.test.js
@@ -3,6 +3,7 @@ import game from './game';
 test('Expect boum to be pow', () => {
   const a = game('boum');
   expect(a).toBe('pow');
+  expect(true).toBe(false);
 });
 
 test('Expect bad input to be empty string', () => {
