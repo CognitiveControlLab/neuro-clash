66d1c8b92496b9c2d0d6115647ddb8ca19f87825
Philippe <diop2408@usherbrooke.ca>
2024-02-03 17:59:52 -0500

    Adjusted live data grapg

--- neuro_ai/experiments/live.py
+++ neuro_ai/experiments/live.py
@@ -1,7 +1,7 @@
 
 import argparse
 import logging
-
+import numpy as np
 import pyqtgraph as pg
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
 from brainflow.data_filter import DataFilter, FilterTypes, DetrendOperations
@@ -39,7 +39,6 @@ class Graph:
         self.chanel_color = ['r', 'b', 'g', 'w', 'y', 'p'] #add colour
         self.sampling_rate = BoardShim.get_sampling_rate(self.board_id)
         self.overlap_plot = overlap_plot
-
         self.app = QtWidgets.QApplication([])
         self.win = pg.GraphicsLayoutWidget(title=title, size=size)
         self.win.show()
@@ -56,10 +55,12 @@ class Graph:
             else:
                 p = self.win.addPlot(row=i, col=0)
             p.addLegend()
-            p.showAxis("left", False)
+            p.showAxis("left", True)
             p.setMenuEnabled("left", False)
-            p.showAxis("bottom", False)
+            p.showAxis("bottom", True)
             p.setMenuEnabled("bottom", False)
+            p.setYRange(-0.01, 0.01, padding=0)
+            #p.setYRange(1, 20, padding=0)
 
             self.plots.append(p)
             curve = p.plot(pen=self.chanel_color[i], name=self.exg_channels_names[i])
@@ -67,27 +68,103 @@ class Graph:
 
     def update(self, data):
         #data = self.board_shim.get_current_board_data(self.num_points)
-        for count, channel in enumerate([0,1]):
+        for i, channel in enumerate(self.exg_channels_names):
             # plot timeseries
-            self.curves[count].setData(data[channel].tolist())
+            self.curves[i].setData(data[i])
 
         self.app.processEvents()
 
 
-
+data1 = []
+data2 = []
+data3 = []
 def process_data(board_shim, graphs=None):
-
     #Get latest data
-    data = board_shim.get_current_board_data(1000)
+    data = board_shim.get_current_board_data(10*256)
+    #transform brainflow to mne data
+    raw = setup_mne_data(data)
     #plot data
-    test = i
-    graphs.graphs['Test1'].update(data)
-    graphs.graphs['Test2'].update(data)
+    sfreq = raw.info["sfreq"]  # Sampling frequency
+    epoch_duration = 1  # Duration of each epoch in seconds
+    epoch_interval = 1  # Interval between epochs in seconds
 
-  
+    # Calculate the number of samples per epoch
+    samples_per_epoch = int(epoch_duration * sfreq)
 
+    # Calculate total number of epochs
+    # TODO: Figure out why sample number seems higher than 60s
+    total_samples = len(raw.times)
+    total_epochs = int(total_samples / (epoch_interval * samples_per_epoch))
 
+    # Create the events array
+    events = np.zeros((total_epochs, 3), int)
+    for i in range(total_epochs):
+        start_sample = i * int(epoch_interval * sfreq)
+        events[i] = [start_sample, 0, 1]  # [time, 0, event_id]
 
+    # Create epochs
+    epochs = mne.Epochs(
+        raw,
+        events=events,
+        event_id=1,
+        tmin=0,
+        tmax=epoch_duration,
+        baseline=None,
+        preload=True,
+    )
+
+    # Normalize the data
+    epochs_data = epochs.get_data()
+    epochs_data_normalized = (epochs_data - np.mean(epochs_data)) / np.std(epochs_data)
+
+    # Calculate PSD with the full frequency range
+    # TODO: What the heck is power supposed to be?
+    psd, freqs = mne.time_frequency.psd_array_welch(
+        epochs_data_normalized, sfreq, fmin=4, fmax=40, n_per_seg=256
+    )
+
+    # Find indices for alpha and beta bands
+    alpha_indices = np.where((freqs >= 8) & (freqs <= 12))[0]
+    beta_indices = np.where((freqs >= 13) & (freqs <= 30))[0]
+    theta_indices = np.where((freqs >= 4) & (freqs <= 7))[0]
+    gamma_indices = np.where((freqs >= 30) & (freqs <= 40))[0]
+
+    # Extract and average the power for alpha and beta bands
+    # alpha_power = psd[:, 0, alpha_indices]
+    # beta_power = psd[:, 0, beta_indices]
+    alpha_power = psd[:, :, alpha_indices].mean(axis=2)
+    beta_power = psd[:, :, beta_indices].mean(axis=2)
+    theta_power = psd[:, :, theta_indices].mean(axis=2)
+    gamma_power = psd[:, :, gamma_indices].mean(axis=2)
+
+    mean_alpha_power = alpha_power.mean(axis=1)
+    mean_beta_power = beta_power.mean(axis=1)
+    mean_theta_power = theta_power.mean(axis=1)
+    mean_gamma_power = gamma_power.mean(axis=1)
+
+    # Generate the epochs array (assuming you have the same number of epochs for both alpha and beta)
+    epochs_array = np.arange(len(mean_alpha_power))
+    data1.append(mean_alpha_power[-1])
+    data2.append(mean_beta_power[-1])
+    data3.append(mean_theta_power[-1])
+
+    graphs.graphs['Test1'].update([data1[-100:],data2[-100:],data3[-100:]])
+    graphs.graphs['Test2'].update([data1,data2])
+
+def setup_mne_data(data):
+    eeg_data = data[[1, 2, 3, 4], :]
+
+    eeg_data = eeg_data / 1000000  # BrainFlow returns uV, convert to V for MNE
+    info = mne.create_info(
+        ch_names=["TP9", "AF7", "AF8", "TP10"], sfreq=256, ch_types=["eeg", "eeg", "eeg", "eeg"]
+    )
+
+    raw = mne.io.RawArray(eeg_data, info)
+    # Add a montage (only necessary for plotting)
+    montage = mne.channels.make_standard_montage("standard_1020")
+    raw.set_montage(montage)
+
+    return raw
 
 def main():
     update_speed_ms = 50
@@ -106,10 +183,10 @@ def main():
         #data = board_shim.get_current_board_data(self.num_points)
         
         #Add graph
-        graphs.add_graph([1,4], 'Test1', True)
-        graphs.add_graph([2,3], 'Test2')
-        # graphs.add_graph(['F1', 'F2'], 'Test1', True)
-        # graphs.add_graph(['T1', 'T2'], 'Test2')
+        #graphs.add_graph([1,4], 'Test1', True)
+        #graphs.add_graph([2,3], 'Test2')
+        graphs.add_graph(['Alpha', 'Beta', 'Teta'], 'Test1', True)
+        graphs.add_graph(['Alpha', 'Beta'], 'Test2')
 
         #Init the timer
         timer = QtCore.QTimer()
