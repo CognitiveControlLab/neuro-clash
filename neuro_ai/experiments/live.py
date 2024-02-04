bc21e6027fc800ae2d2e061ca7277c1bb389ce02
Philippe <diop2408@usherbrooke.ca>
2024-02-03 19:55:45 -0500

    Created data processing class

--- neuro_ai/experiments/live.py
+++ neuro_ai/experiments/live.py
@@ -59,7 +59,7 @@ class Graph:
             p.setMenuEnabled("left", False)
             p.showAxis("bottom", True)
             p.setMenuEnabled("bottom", False)
-            p.setYRange(-0.01, 0.01, padding=0)
+            p.setYRange(0, 1000, padding=0)
             #p.setYRange(1, 20, padding=0)
 
             self.plots.append(p)
@@ -78,83 +78,71 @@ class Graph:
 data1 = []
 data2 = []
 data3 = []
-def process_data(board_shim, graphs=None):
-    #Get latest data
-    data = board_shim.get_current_board_data(10*256)
-    #transform brainflow to mne data
-    raw = setup_mne_data(data)
-    #plot data
-    sfreq = raw.info["sfreq"]  # Sampling frequency
-    epoch_duration = 1  # Duration of each epoch in seconds
-    epoch_interval = 1  # Interval between epochs in seconds
 
-    # Calculate the number of samples per epoch
-    samples_per_epoch = int(epoch_duration * sfreq)
+class DataProcessig:
+    def __init__(self):
 
-    # Calculate total number of epochs
-    # TODO: Figure out why sample number seems higher than 60s
-    total_samples = len(raw.times)
-    total_epochs = int(total_samples / (epoch_interval * samples_per_epoch))
+        #add wave data
+        self.len_wave_hist = 10
+        self.wave_data = {}
+        self.wave_data['waves_avg'] = {}
+        self.wave_data['waves_avg']['alpha'] = []
+        self.wave_data['waves_avg']['beta'] = []
+        self.wave_data['waves_avg']['theta'] = []
+        self.wave_data['waves_avg']['gamma'] = []
 
-    # Create the events array
-    events = np.zeros((total_epochs, 3), int)
-    for i in range(total_epochs):
-        start_sample = i * int(epoch_interval * sfreq)
-        events[i] = [start_sample, 0, 1]  # [time, 0, event_id]
+    def append_wave_avg(self, a, b, t, g):
+        self.wave_data['waves_avg']['alpha'].append(a)
+        self.wave_data['waves_avg']['alpha'] = self.wave_data['waves_avg']['alpha'][-self.len_wave_hist:]
+        self.wave_data['waves_avg']['beta'].append(b)
+        self.wave_data['waves_avg']['beta'] = self.wave_data['waves_avg']['beta'][-self.len_wave_hist:]
+        self.wave_data['waves_avg']['theta'].append(t)
+        self.wave_data['waves_avg']['theta'] = self.wave_data['waves_avg']['theta'][-self.len_wave_hist:]
+        self.wave_data['waves_avg']['gamma'].append(g)
+        self.wave_data['waves_avg']['gamma'] = self.wave_data['waves_avg']['gamma'][-self.len_wave_hist:]
 
-    # Create epochs
-    epochs = mne.Epochs(
-        raw,
-        events=events,
-        event_id=1,
-        tmin=0,
-        tmax=epoch_duration,
-        baseline=None,
-        preload=True,
-    )
+    def process_data(self, board_shim, graphs=None):
+        #Get latest data
+        data = board_shim.get_current_board_data(256)
+        #transform brainflow to mne data
+        raw = setup_mne_data(data)
+        #plot data
+        sfreq = raw.info["sfreq"]  # Sampling frequency
 
-    # Normalize the data
-    epochs_data = epochs.get_data()
-    epochs_data_normalized = (epochs_data - np.mean(epochs_data)) / np.std(epochs_data)
+        psd, freqs = mne.time_frequency.psd_array_welch(
+            raw.get_data(), sfreq, fmin=4, fmax=40, n_per_seg=256
+        )
 
-    # Calculate PSD with the full frequency range
-    # TODO: What the heck is power supposed to be?
-    psd, freqs = mne.time_frequency.psd_array_welch(
-        epochs_data_normalized, sfreq, fmin=4, fmax=40, n_per_seg=256
-    )
+        # Find indices for alpha and beta bands
+        alpha_indices = np.where((freqs >= 8) & (freqs <= 12))[0]
+        beta_indices = np.where((freqs >= 13) & (freqs <= 30))[0]
+        theta_indices = np.where((freqs >= 4) & (freqs <= 7))[0]
+        gamma_indices = np.where((freqs >= 30) & (freqs <= 40))[0]
 
-    # Find indices for alpha and beta bands
-    alpha_indices = np.where((freqs >= 8) & (freqs <= 12))[0]
-    beta_indices = np.where((freqs >= 13) & (freqs <= 30))[0]
-    theta_indices = np.where((freqs >= 4) & (freqs <= 7))[0]
-    gamma_indices = np.where((freqs >= 30) & (freqs <= 40))[0]
+        # Extract and average the power for alpha and beta bands
+        # alpha_power = psd[:, 0, alpha_indices]
+        # beta_power = psd[:, 0, beta_indices]
+        alpha_power = psd[:, alpha_indices].mean(axis=1)
+        beta_power = psd[:, beta_indices].mean(axis=1)
+        theta_power = psd[:, theta_indices].mean(axis=1)
+        gamma_power = psd[:, gamma_indices].mean(axis=1)
 
-    # Extract and average the power for alpha and beta bands
-    # alpha_power = psd[:, 0, alpha_indices]
-    # beta_power = psd[:, 0, beta_indices]
-    alpha_power = psd[:, :, alpha_indices].mean(axis=2)
-    beta_power = psd[:, :, beta_indices].mean(axis=2)
-    theta_power = psd[:, :, theta_indices].mean(axis=2)
-    gamma_power = psd[:, :, gamma_indices].mean(axis=2)
+        mean_alpha_power = alpha_power.mean(axis=0)
+        mean_beta_power = beta_power.mean(axis=0)
+        mean_theta_power = theta_power.mean(axis=0)
+        mean_gamma_power = gamma_power.mean(axis=0)
 
-    mean_alpha_power = alpha_power.mean(axis=1)
-    mean_beta_power = beta_power.mean(axis=1)
-    mean_theta_power = theta_power.mean(axis=1)
-    mean_gamma_power = gamma_power.mean(axis=1)
+        # Generate the epochs array (assuming you have the same number of epochs for both alpha and beta)
+        print(mean_alpha_power)
+        self.append_wave_avg(mean_alpha_power, mean_beta_power, mean_theta_power, mean_gamma_power)
 
-    # Generate the epochs array (assuming you have the same number of epochs for both alpha and beta)
-    epochs_array = np.arange(len(mean_alpha_power))
-    data1.append(mean_alpha_power[-1])
-    data2.append(mean_beta_power[-1])
-    data3.append(mean_theta_power[-1])
-
-    graphs.graphs['Test1'].update([data1[-100:],data2[-100:],data3[-100:]])
-    graphs.graphs['Test2'].update([data1,data2])
+        graphs.graphs['Test1'].update([self.wave_data['waves_avg']['alpha'], self.wave_data['waves_avg']['beta'], self.wave_data['waves_avg']['theta']])
+        #graphs.graphs['Test2'].update([data1,data2])
 
 def setup_mne_data(data):
     eeg_data = data[[1, 2, 3, 4], :]
 
-    eeg_data = eeg_data / 1000000  # BrainFlow returns uV, convert to V for MNE
+    #eeg_data = eeg_data / 1000000  # BrainFlow returns uV, convert to V for MNE
     info = mne.create_info(
         ch_names=["TP9", "AF7", "AF8", "TP10"], sfreq=256, ch_types=["eeg", "eeg", "eeg", "eeg"]
     )
@@ -167,7 +155,7 @@ def setup_mne_data(data):
     return raw
 
 def main():
-    update_speed_ms = 50
+    update_speed_ms = 1000
     BoardShim.enable_dev_board_logger()
     logging.basicConfig(level=logging.DEBUG)
 
@@ -187,11 +175,12 @@ def main():
         #graphs.add_graph([2,3], 'Test2')
         graphs.add_graph(['Alpha', 'Beta', 'Teta'], 'Test1', True)
         graphs.add_graph(['Alpha', 'Beta'], 'Test2')
+        dp = DataProcessig()
 
         #Init the timer
         timer = QtCore.QTimer()
         #process_data is the fct that will be called by the API instead
-        timer.timeout.connect(lambda:process_data(board_shim, graphs))
+        timer.timeout.connect(lambda:dp.process_data(board_shim, graphs))
         timer.start(update_speed_ms)
         QtWidgets.QApplication.instance().exec_()
 
