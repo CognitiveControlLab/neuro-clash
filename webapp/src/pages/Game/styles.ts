f7aeaf260723efa911d962d48e43cb2a43d824dd
Georges Mainville <g.mainville@mphase.ca>
2024-02-04 16:33:30 -0500

    Add with  good names

---
+++ webapp/src/pages/Game/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const GameScore = styled.div`
+  display: flex;
+  flex: 1;
+  font-size: 3rem;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
