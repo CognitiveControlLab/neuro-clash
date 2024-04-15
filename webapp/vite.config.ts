a43f587a94b71babaeda37f11418600b4fac317b
kozeh <anthony.r@hotmail.fr>
2024-04-14 20:57:17 -0400

    Update port number in index.ts and socket.ts files

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-
-dotenv.config(); // Load the .env file
 
 export default defineConfig({
   plugins: [react()],
   // Patch dev1
   define: {
-    'process.env.PORT': process.env.PORT || 5001,
     ...(process.env.APP_ENV === 'production' ? {} : { global: {} }),
   },
 });
