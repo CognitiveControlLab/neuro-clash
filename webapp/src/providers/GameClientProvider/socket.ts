1b8af534e41acdfaa5b82dbd3e65461a075501f0
Etienne Simoneau <etiennesimoneau@hotmail.fr>
2024-04-24 20:04:11 -0400

    Fix web app infinite reload

--- webapp/src/providers/GameClientProvider/socket.ts
+++ webapp/src/providers/GameClientProvider/socket.ts
@@ -1,10 +1,10 @@
 import { io } from 'socket.io-client';
 
 // @ts-ignore : This is a global variable injected by Vite.
-const host = import.meta.env.NEURO_CLASH_API_HOST ?? '127.0.0.1';
+const host = import.meta.env.VITE_NEURO_CLASH_API_HOST ?? '127.0.0.1';
 
 // @ts-ignore : This is a global variable injected by Vite.
-const port = import.meta.env.NEURO_CLASH_API_PORT ?? 5000;
+const port = import.meta.env.VITE_NEURO_CLASH_API_PORT ?? 5000;
 
 const socket = io(`ws://${host}:${port}`, { transports: ['websocket'] });
 
